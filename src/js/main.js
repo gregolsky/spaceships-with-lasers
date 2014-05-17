956dab3d74e075bdaadf79df9680abdfdce3057d
Grzegorz Lachowski <gregory.lachowski@gmail.com>
2014-05-17 21:11:57 +0200

    impl: blink insert coin text

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,7 @@ window.onload = function () {
   var game
     , ns = window['spaceships-with-lasers'];
 
-  game = new Phaser.Game(640, 480, Phaser.AUTO, 'spaceships-with-lasers-game');
+  game = new Phaser.Game(1024, 768, Phaser.AUTO, 'spaceships-with-lasers-game');
   game.state.add('boot', ns.Boot);
   game.state.add('preloader', ns.Preloader);
   game.state.add('menu', ns.Menu);
--- src/js/menu.js
+++ src/js/menu.js
@@ -10,15 +10,16 @@
 
     create: function () {
       var x = this.game.width / 2
-        , y = this.game.height / 2;
+        , y = this.game.height / 2 - 50;
 
 
-      this.titleTxt = this.add.bitmapText(x, y, 'minecraftia', 'Example Game' );
+      this.timer = 0;
+        
+      this.titleTxt = this.add.bitmapText(x, y, 'minecraftia', 'Spaceships. With lasers.' );
       this.titleTxt.align = 'center';
       this.titleTxt.x = this.game.width / 2 - this.titleTxt.textWidth / 2;
 
-      y = y + this.titleTxt.height + 5;
-      this.startTxt = this.add.bitmapText(x, y, 'minecraftia', 'START');
+      this.startTxt = this.add.bitmapText(x, y + 50, 'minecraftia', 'INSERT COIN');
       this.startTxt.align = 'center';
       this.startTxt.x = this.game.width / 2 - this.startTxt.textWidth / 2;
 
@@ -26,7 +27,14 @@
     },
 
     update: function () {
-
+        
+        var interval = 500;
+        this.timer += this.game.time.elapsed; //this is in ms, not seconds.
+        if (this.timer >= interval)
+        {
+            this.timer -= interval;
+            this.startTxt.visible = !this.startTxt.visible;
+        }
     },
 
     onDown: function () {
